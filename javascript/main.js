9676c745565f826350ce1b803feb90406ffccaef
dilione <143102631+dilione-junior@users.noreply.github.com>
2024-02-05 00:32:17 -0300

    Add files via upload

---
+++ javascript/main.js
@@ -0,0 +1,34 @@
+function executarCriptografia() {
+    const mensagemSecreta = prompt("Escreva sua mensagem secreta");
+    console.log(mensagemSecreta);
+
+    function mostrarResultado(frase) {
+        alert(frase);
+    }
+
+    function cifraMensagem(mensagem, movimentos) {
+        const mensagemCifrada = mensagem.split('').map(caractere => {
+            const codigoCaractere = caractere.charCodeAt(0);
+            return String.fromCharCode(codigoCaractere + movimentos);
+        });
+
+        return mensagemCifrada.join('');
+    }
+
+    const mensagemCifrada = cifraMensagem(mensagemSecreta, 3);
+    console.log(mensagemCifrada);
+
+    function decifraMensagem(mensagem, movimentos) {
+        const mensagemCifrada = mensagem.split('').map(caractere => {
+            const codigoCaractere = caractere.charCodeAt(0);
+            return String.fromCharCode(codigoCaractere - movimentos);
+        });
+
+        return mensagemCifrada.join('');
+    }
+
+    const mensagemDecifrada = decifraMensagem(mensagemCifrada, 3);
+    console.log(mensagemDecifrada);
+
+    mostrarResultado("Sua mensagem criptografada é " + mensagemCifrada);
+}
